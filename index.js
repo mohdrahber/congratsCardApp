1ff4137c9ac1933f5f54a8cca69526b57b622ab0
mohdrahber <mohdrahberamu@gmail.com>
2022-02-17 04:50:12 +0000

    css and js file

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+    <div className = "bg-container">
+        <h1 className = "heading">Congratulation</h1>
+        <div className = "card-container">
+            <img className = "image" src = "https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+            <h1 className = "name">Kiran V</h1>
+            <p>Zakir Hussain College of Engineering and Technology,</br>Aligarh Muslim University</p>
+            <img className = "image" src  = "https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+        </div>
+    </div>
+  )
+  
+ReactDOM.render(element, document.getElementById('root'))
